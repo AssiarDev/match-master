2b5325b27c0cfe064332eb89cc8ab262469fb7c3
AssiarDev <halibaraissa@gmail.com>
2025-05-01 12:10:17 +0200

    [ADD] route logout et express session

--- server/routes/login.js
+++ server/routes/login.js
@@ -12,11 +12,13 @@ router.post('/login', async (req, res) => {
             return res.status(400).json({ error: "Tous les champs sont obligatoires" });
         }
 
-        if (user) {
-            res.status(201).json({ message: "Connexion effectué avec succès.", token : user.token });
+        if (user.success) {
+            req.session.user = { id: user.id, email: user.email, token: user.token };
+            res.status(200).json({ message: "Connexion réussie.", token: user.token });
         } else {
-            res.status(409).json({ error: user.message || "Echec lors de la connexion" });
+            res.status(401).json({ error: "Identifiants incorrects." });
         }
+
     } catch(e){
         res.status(500).json({ error: "Erreur serveur" });
     }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import session from 'express-session';
 import { initializeDatabase } from './db/db.js';
 import { teams } from './routes/teams.js';
 import { competitions } from './routes/competitions.js';
 import { standings } from './routes/standings.js';
 import { users } from './routes/users.js';
 import { login } from './routes/login.js';
+import { logout } from './routes/logout.js';
 
 const app = express();
 const port = process.env.PORT;
@@ -21,11 +23,20 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 app.use(express.json());
+
+app.use(session({
+    secret: process.env.SESSION_KEY, 
+    resave: false, 
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 3600000 } 
+}));
+
 app.use(teams);
 app.use(competitions);
-app.use(standings)
-app.use(users)
-app.use(login)
+app.use(standings);
+app.use(users);
+app.use(login);
+app.use(logout)
 
 app.get('/', (req, res) => {
     res.send('Hello from Express');
