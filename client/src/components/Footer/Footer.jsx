d9ef0a309669188d7dbd2bfc4f051407dff06c8f
AssiarDev <halibaraissa@gmail.com>
2025-05-12 09:51:29 +0200

    [ADD] footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+    return (
+        <footer className="w-full bg-neutral-900 text-white text-center py-4 mt-10">
+            <p className="text-sm">&copy; {new Date().getFullYear()} - Tous droits réservés</p>
+            <p className="text-sm">Développé par Raïssa Ali</p>
+        </footer>
+    )
+}
