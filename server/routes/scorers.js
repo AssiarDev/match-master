ff9171d81adaf6984c3a4b256b77a57e08bebefd
AssiarDev <halibaraissa@gmail.com>
2025-05-20 14:24:29 +0200

    [FIX] route scorers

--- server/routes/scorers.js
+++ server/routes/scorers.js
@@ -4,14 +4,15 @@ import { topScorers } from '../api/api.js';
 const router = express.Router(); 
 
 router.get('/scorers/:id', async (req, res) => {
-    const id = req.params.id;
-    try {
-        const result = await topScorers(id)
-        res.json(result)
-    } catch(e){
-        console.error('Erreur, impossible de récupérer les meilleurs buteurs', e.message)
-        res.status(500).send('Error fetching data')
-    }
+    res.send("test");
+    // const id = req.params.id;
+    // try {
+    //     const result = await topScorers(id)
+    //     res.json(result)
+    // } catch(e){
+    //     console.error('Erreur, impossible de récupérer les meilleurs buteurs', e.message)
+    //     res.status(500).send('Error fetching data')
+    // }
 });
 
 export { router as scorers }
